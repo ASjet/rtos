d2bb6caacbbb8b949d418099abcd6ae1dd7ad683
ASjet <Aryan.ITFS@iCloud.com>
2021-04-11 21:06:57 +0800

    fix memory leak issue

--- applications/printer.c
+++ applications/printer.c
@@ -1,11 +1,27 @@
 #include <rtthread.h>
 
+// 发送完一个打印任务后是否延迟
+// #define DELAY_AFTER_SEND
+
+// 打印完毕后是否删除打印机线程
+// #define DELETE_AFTER_PRINT
+
+// 线程参数
 #define THREAD_PRIORITY (rt_int8_t)25
 #define THREAD_STACK_SIZE (rt_int32_t)512
 #define THREAD_TIMESLICE (rt_int32_t)10
+
+// 单次打印内容最大长度
 #define PRINT_CONTENT_SIZE (rt_size_t)128
+
+// 打印队列长度
 #define PRINT_QUEUE_LENGTH (rt_size_t)4
-#define SENDER_DELAY (rt_int32_t)100
+
+// 发送任务延时长度
+#define SENDER_DELAY (rt_int32_t)10
+
+// 等待时间长度
+#define WAIT_TIME (rt_int32_t)100
 
 typedef struct
 {
@@ -14,40 +30,48 @@ typedef struct
 } print_task;
 typedef print_task *print_task_t;
 
+typedef struct
+{
+    rt_size_t amount;
+    rt_size_t remain;
+    char *tasks[];
+} task_batch;
+
 static rt_mailbox_t print_queue = RT_NULL;
-
-static rt_thread_t printer = RT_NULL;
-
-#define TASK_COUNT 2
-
-static rt_thread_t sender1 = RT_NULL;
-static rt_thread_t sender2 = RT_NULL;
-
-int task_barrier = TASK_COUNT;
-
 static rt_mutex_t print = RT_NULL;
 static rt_sem_t print_queue_full = RT_NULL;
 static rt_sem_t print_queue_empty = RT_NULL;
 
-static char *task1[] = {
+static rt_thread_t printer = RT_NULL;
+
+// 初始化批处理信息
+#define SENDER_AMOUNT 2
+
+static rt_thread_t sender1 = RT_NULL;
+static task_batch si1 = {5, 5, {
     "First task of sender1",
     "Second task of sender1",
     "Third task of sender1",
     "Forth task of sender1",
-    "Fifth task of sender1"};
+    "Fifth task of sender1"}};
 
-static char *task2[] = {
+static rt_thread_t sender2 = RT_NULL;
+static task_batch si2 = {2, 2, {
     "First task of sender2",
-    "Second task of sender2"};
+    "Second task of sender2"}};
+
+static int sender_barrier = SENDER_AMOUNT;
 
 static void sender1_sig_handler(int _Signal)
 {
-    rt_kprintf("Sender1: Print task done.\n");
+    --si1.remain;
+    rt_kprintf("Sender1: Print task done.(%d/%d)\n",si1.amount-si1.remain,si1.amount);
 }
 
 static void sender2_sig_handler(int _Signal)
 {
-    rt_kprintf("Sender2: Print task done.\n");
+    --si2.remain;
+    rt_kprintf("Sender2: Print task done.(%d/%d)\n",si2.amount-si2.remain,si2.amount);
 }
 
 static void printer_sig_handler(int _Signal)
@@ -69,7 +93,7 @@ static void printerEntry(void *_Parameter)
     while (1)
     {
         if (rt_sem_take(print_queue_full, RT_WAITING_FOREVER) != RT_EOK)
-            rt_kprintf("Printer: Failed to receive print semaphore.\n");
+            continue;
         if (rt_mb_recv(print_queue, (rt_uint32_t *)&task, RT_WAITING_FOREVER) != RT_EOK)
             rt_kprintf("Printer: Failed to receive print task mail.\n");
 
@@ -80,9 +104,6 @@ static void printerEntry(void *_Parameter)
         rt_kprintf("%s\n", task->content);
         rt_kprintf("-----Print Done------\n");
 
-        // 回收内存
-        // rt_free(task);
-
         // 发送打印完成信号
         rt_thread_kill(task->tid, SIGUSR1);
     }
@@ -90,11 +111,14 @@ static void printerEntry(void *_Parameter)
 
 static void senderEntry(void *_Parameter)
 {
-    int i = 0, task_len = 0;
-    char **content_list = RT_NULL;
-    rt_int32_t sleep_time = RT_NULL;
+    int i = 0;
     print_task_t task_list = RT_NULL;
+    task_batch *info = RT_NULL;
     rt_thread_t tid = rt_thread_self();
+
+#ifdef DELAY_AFTER_SEND
+    rt_int32_t delay = RT_NULL;
+#endif
 
     // 唤醒打印机线程
     if ((printer->stat & RT_THREAD_STAT_MASK) == RT_THREAD_SUSPEND)
@@ -107,16 +131,22 @@ static void senderEntry(void *_Parameter)
     if (tid == sender1)
     {
         rt_signal_install(SIGUSR1, sender1_sig_handler);
-        task_len = 5;
-        content_list = task1;
-        sleep_time = SENDER_DELAY;
+        info = &si1;
+
+#ifdef DELAY_AFTER_SEND
+        delay = SENDER_DELAY;
+#endif
+
     }
     else if (tid == sender2)
     {
         rt_signal_install(SIGUSR1, sender2_sig_handler);
-        task_len = 2;
-        content_list = task2;
-        sleep_time = SENDER_DELAY * 2;
+        info = &si2;
+
+#ifdef DELAY_AFTER_SEND
+        delay = SENDER_DELAY * 2;
+#endif
+
     }
     else
     {
@@ -125,20 +155,23 @@ static void senderEntry(void *_Parameter)
     }
     rt_signal_unmask(SIGUSR1);
 
-    task_list = (print_task_t)rt_malloc(sizeof(print_task) * task_len);
+    task_list = (print_task_t)rt_malloc(sizeof(print_task) * info->amount);
     if (task_list == NULL)
     {
-        rt_kprintf("Error: Unable to allocate memory.\n");
+        rt_kprintf("Error: Unable to allocate memory for task list.\n");
     }
 
     // 发送任务
-    for (i = 0; i < task_len; ++i)
+    for (i = 0; i < info->amount; ++i)
     {
-        task_list[i].content = content_list[i];
+        task_list[i].content = info->tasks[i];
         task_list[i].tid = tid;
 
         if (rt_mutex_take(print, RT_WAITING_FOREVER) != RT_EOK)
+        {
             rt_kprintf("Error: Deadlock.\n");
+            goto _exit;
+        }
         rt_sem_take(print_queue_empty, RT_WAITING_FOREVER);
 
         rt_mb_send_wait(print_queue, (rt_uint32_t)(task_list + i), RT_WAITING_FOREVER);
@@ -146,14 +179,29 @@ static void senderEntry(void *_Parameter)
         rt_sem_release(print_queue_full);
         rt_mutex_release(print);
 
+#ifdef DELAY_AFTER_SEND
+        rt_thread_mdelay(delay);
+#endif
+
     }
-    --task_barrier;
-    rt_thread_mdelay(sleep_time);
+
+    // 等待任务队列完成
+    while (info->remain > 0)
+        rt_thread_mdelay(WAIT_TIME);
+
+_exit:
+    rt_free(task_list);
+    --sender_barrier;
     // 若全部任务线程均到达屏障则休眠打印机
-    if (task_barrier == 0)
+    if (sender_barrier == 0)
     {
         rt_thread_kill(printer, SIGUSR2);
+
+#ifdef DELETE_AFTER_PRINT
+        rt_thread_mdelay(WAIT_TIME * 10);
         rt_thread_delete(printer);
+#endif
+
     }
 }
 
